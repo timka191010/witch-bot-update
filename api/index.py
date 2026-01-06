e23561d26adffa5348551465fcdb6c1914b349e9
timka191010 <130147481+timka191010@users.noreply.github.com>
2026-01-07 04:49:48 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -258,4 +258,6 @@ def not_found(e):
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
