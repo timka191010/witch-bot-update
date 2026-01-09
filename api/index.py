01296c4086cc419ef41419f96457fbd599737ff9
timka191010 <130147481+timka191010@users.noreply.github.com>
2026-01-09 18:24:13 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -330,4 +330,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
