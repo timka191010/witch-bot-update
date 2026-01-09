fa3eb46baa219d2d280b821fe924698a56804e28
timka191010 <130147481+timka191010@users.noreply.github.com>
2026-01-09 16:44:08 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -74,4 +74,4 @@ def server_error(error):
     return jsonify({'error': 'Server Error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0', port 8080)
